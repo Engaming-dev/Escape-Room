9569c4c3af0afe0c546aa982ab075061b10ab6e3
Enes Gjikokaj <75257483+Engaming-dev@users.noreply.github.com>
2021-06-15 15:29:11 +0000

    Added a new level, specifically, room 2. This room is tougher than the first one. Also added a points system, witch adds one point every room you escape. If you make more points then your personal best, you can publish it as a high score. The publishing part is currently not available, but will be added as time goes on.

--- main.rb
+++ main.rb
@@ -4,7 +4,7 @@ puts "Welcome to Room 1. Your objective is to escape the room. there are puzzles
 puts "Your main way to escape is to find the key."
 puts "----->" "{}"
 puts "This is the keyhole." 
-puts "the code is 6 digits long. You can find it somewhere hidden. You will need to find it to find the key. Notice that other rooms might have other measures to not allow you to access the key."
+puts "the code is 6 digits long. You can find it somewhere hidden. You will need to find it to find the key. Notice that other rooms might have other measures to not allow you to access the key, and that the code in other rooms might be longer."
 puts "Lets check the drawer."
 puts "CODE: 139786"
 puts "Now that you know the code, lets go enter it in."
@@ -21,7 +21,8 @@ if code == "139786"
    if key1 =="{}"
     puts "The door opens."
     puts "You Have Escaped!"
-    puts "More rooms coming soon!"
+    points = 1
+    puts "You have completed the tutorial! As a result, you have earned a point. You will unlock other rooms with points. Every room you beat you will earn a point. When you lose, your points will act as your high score."
    else
      puts "The key did not work."
    end
@@ -32,3 +33,34 @@ else
   puts "ACCESS DENIED. CODE INCORRECT."
   puts "You have failed! Try again."
 end
+if points == 1
+  puts "      ------ROOM 2------     "
+  puts "Congratulations on making to room 2!"
+  puts "----> [;]"
+  puts "Do you want to inspect for clues?"
+  answer = gets.chomp.capitalize
+  if answer == "Yes"
+    puts "You inspect for clues."
+    puts "You find a clue to the code!"
+    puts "THE CODE: The first triple-digit number, the last quadruple-digit number, the first double-digit number"
+    puts "Can you figure out the code? Type it in!"
+    code = gets.chomp
+    if code == "100999910"
+      puts "ACCESS GRANTED."
+      puts "You found a key! It works for this keyhole:[;]"
+      puts "Do you want to use the key?" 
+      answer = gets.chomp.capitalize
+      if answer == "Yes"
+        puts "The door opens."
+        puts "You Have Escaped!"
+        points = 2
+        puts "Your score: #{points}. If you havent made it farther, you can publish this as a high score!"
+      else
+        puts "You dont use the key."
+        puts "Your score: #{points}. If you havent made it farther, you can publish this as a high score!"
+      end
+  else
+    puts "You dont inspect the room, and remain clueless."
+    puts "Your score: #{points}. If you havent made it farther, you can publish this as a high score!"  
+  end
+end end
